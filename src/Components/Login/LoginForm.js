a723ac6e9fcc06f65d035f5193ab7761b6d5b8c3
gustavobarsan <gustavobarsan@gmail.com>
2022-08-22 14:49:25 -0300

    chore: API fetch login

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,36 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import URL from "../../api/URL";
+// import URL from "../../api/URL";
 import { Input } from "../Forms/Input";
 import { Button } from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
-
+import { TOKEN_POST, USER_GET } from "../../api";
 export const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(event) {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  });
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const res = await fetch(url, options);
+    const json = await res.json();
+    console.log(json);
+  }
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch(`${URL}/jwt-auth/v1/token`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username.value,
-          password: password.value,
-        }),
-      })
-        .then((res) => {
-          console.log(res);
-          return res.json();
-        })
-        .then((json) => {
-          console.log(json);
-          return json;
-        });
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+      const res = await fetch(url, options);
+      const json = await res.json();
+      console.log(json);
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
     }
   }
 
